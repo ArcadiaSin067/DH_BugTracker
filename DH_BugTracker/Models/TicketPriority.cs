e35442af60a7a1be7b060bc0134db9c5fd6f29a2
ArcadiaSin067 <52840132+ArcadiaSin067@users.noreply.github.com>
2019-10-24 16:38:10 -0400

    all set up til carving out a template

--- DH_BugTracker/Models/TicketPriority.cs
+++ DH_BugTracker/Models/TicketPriority.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace DH_BugTracker.Models
     public class TicketPriority
     {
         public int Id { get; set; }
+
+        [Display(Name = "Ticket Priority")]
         public string Name { get; set; }
         public string Description { get; set; }
 
--- DH_BugTracker/Models/TicketStatus.cs
+++ DH_BugTracker/Models/TicketStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace DH_BugTracker.Models
     public class TicketStatus
     {
         public int Id { get; set; }
+
+        [Display(Name = "Ticket Status")]
         public string Name { get; set; }
         public string Description { get; set; }
 
